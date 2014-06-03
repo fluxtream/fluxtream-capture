92606f70f15f369537fedb5b0b3d7a680efad86d
Yury Chernushenko <Yury@Yurys-MacBook-Pro.local>
2014-06-03 17:52:31 -0400

    Added Topics screen and footer

--- fluxtreamcapture/src/scripts/applications/self-report/App.js
+++ fluxtreamcapture/src/scripts/applications/self-report/App.js
@@ -12,11 +12,16 @@ define(["core/Application", "core/FlxState", "env"], function(Application, FlxSt
         require(["text!applications/self-report/" + state + ".html"], function (otherTemplate) {
             forge.logging.info(otherTemplate);
         });
+
+        //SHOW OBSERVATION SCREEN
     }
 
     SelfReport.angularSetup = function() {
         App.angularApp
             .controller('SelfReportController', ['$scope', function ($scope) {
+
+                $("h1.title").html("Create Observation");
+
                 forge.logging.debug("retrieving guest model...");
                 var handleSuccess = function(guestModel, textStatus) {
                     forge.logging.debug(guestModel);
@@ -56,9 +61,188 @@ define(["core/Application", "core/FlxState", "env"], function(Application, FlxSt
                         });
                     }
                 }
-                $scope.goSomewhereElse = function() {
-                    FlxState.router.navigate("self-report/somewhereElse", {trigger: true});
-                }
+
+                $scope.createObservation = function(topicId) {
+                    event.preventDefault();
+                    FlxState.router.navigate("self-report/createObservation", {trigger: true});
+                };
+
+                $scope.topics = [
+                    {"id": 1, "guestId": 1, "creationTime": 12312, "timeUpdated": 123, "name": "Weight1", "type": 1, "step": 1, "defaultValue": 1, "status": 1},
+                    {"id": 2, "guestId": 1, "creationTime": 12312, "timeUpdated": 123, "name": "Weight2", "type": 1, "step": 1, "defaultValue": 1, "status": 1},
+                    {"id": 3, "guestId": 1, "creationTime": 12312, "timeUpdated": 123, "name": "Food1", "type": 1, "step": 1, "defaultValue": 1, "status": 1},
+                    {"id": 4, "guestId": 1, "creationTime": 12312, "timeUpdated": 123, "name": "Food2", "type": 1, "step": 1, "defaultValue": 1, "status": 1},
+                    {"id": 5, "guestId": 1, "creationTime": 12312, "timeUpdated": 123, "name": "Food3", "type": 1, "step": 1, "defaultValue": 1, "status": 1},
+                    {"id": 6, "guestId": 1, "creationTime": 12312, "timeUpdated": 123, "name": "Light1", "type": 1, "step": 1, "defaultValue": 1, "status": 1},
+                    {"id": 7, "guestId": 1, "creationTime": 12312, "timeUpdated": 123, "name": "Food4", "type": 1, "step": 1, "defaultValue": 1, "status": 1}
+                ];
+                //$http.get('/topics.json');
+
+                $scope.observations = [
+                    {
+                        'id': "1",
+                        'api': "1",
+                        'comment': "qqq",
+                        'end': "123",
+                        'fullTextDescription': "qeqweqw",
+                        'guestId': "1",
+                        'isEmpty': "1",
+                        'objectType': "1",
+                        'start': "1231231",
+                        'apiKeyId': "1",
+                        'timeUpdated': "123123",
+                        'tags': "1231231",
+                        'creationTime': "123123",
+                        'topicID': "1",
+                        'timezoneOffset': "123123",
+                        'value': "1"
+                    },
+                    {
+                        'id': "2",
+                        'api': "1",
+                        'comment': "qqq",
+                        'end': "123",
+                        'fullTextDescription': "qeqweqw",
+                        'guestId': "1",
+                        'isEmpty': "1",
+                        'objectType': "1",
+                        'start': "1231231",
+                        'apiKeyId': "1",
+                        'timeUpdated': "123123",
+                        'tags': "1231231",
+                        'creationTime': "123123",
+                        'topicID': "1",
+                        'timezoneOffset': "123123",
+                        'value': "4"
+                    },
+                    {
+                        'id': "3",
+                        'api': "1",
+                        'comment': "qqq",
+                        'end': "123",
+                        'fullTextDescription': "qeqweqw",
+                        'guestId': "1",
+                        'isEmpty': "1",
+                        'objectType': "1",
+                        'start': "1231231",
+                        'apiKeyId': "1",
+                        'timeUpdated': "123123",
+                        'tags': "1231231",
+                        'creationTime': "123123",
+                        'topicID': "2",
+                        'timezoneOffset': "123123",
+                        'value': "2"
+                    },
+                    {
+                        'id': "4",
+                        'api': "1",
+                        'comment': "qqq",
+                        'end': "123",
+                        'fullTextDescription': "qeqweqw",
+                        'guestId': "1",
+                        'isEmpty': "1",
+                        'objectType': "1",
+                        'start': "1231231",
+                        'apiKeyId': "1",
+                        'timeUpdated': "123123",
+                        'tags': "1231231",
+                        'creationTime': "123123",
+                        'topicID': "2",
+                        'timezoneOffset': "123123",
+                        'value': "12"
+                    },
+                    {
+                        'id': "5",
+                        'api': "1",
+                        'comment': "qqq",
+                        'end': "123",
+                        'fullTextDescription': "qeqweqw",
+                        'guestId': "1",
+                        'isEmpty': "1",
+                        'objectType': "1",
+                        'start': "1231231",
+                        'apiKeyId': "1",
+                        'timeUpdated': "123123",
+                        'tags': "1231231",
+                        'creationTime': "123123",
+                        'topicID': "3",
+                        'timezoneOffset': "123123",
+                        'value': "3"
+                    },
+                    {
+                        'id': "6",
+                        'api': "1",
+                        'comment': "qqq",
+                        'end': "123",
+                        'fullTextDescription': "qeqweqw",
+                        'guestId': "1",
+                        'isEmpty': "1",
+                        'objectType': "1",
+                        'start': "1231231",
+                        'apiKeyId': "1",
+                        'timeUpdated': "123123",
+                        'tags': "1231231",
+                        'creationTime': "123123",
+                        'topicID': "4",
+                        'timezoneOffset': "123123",
+                        'value': "0"
+                    },
+                    {
+                        'id': "7",
+                        'api': "1",
+                        'comment': "qqq",
+                        'end': "123",
+                        'fullTextDescription': "qeqweqw",
+                        'guestId': "1",
+                        'isEmpty': "1",
+                        'objectType': "1",
+                        'start': "1231231",
+                        'apiKeyId': "1",
+                        'timeUpdated': "123123",
+                        'tags': "1231231",
+                        'creationTime': "123123",
+                        'topicID': "5",
+                        'timezoneOffset': "123123",
+                        'value': "13"
+                    },
+                    {
+                        'id': "8",
+                        'api': "1",
+                        'comment': "qqq",
+                        'end': "123",
+                        'fullTextDescription': "qeqweqw",
+                        'guestId': "1",
+                        'isEmpty': "1",
+                        'objectType': "1",
+                        'start': "1231231",
+                        'apiKeyId': "1",
+                        'timeUpdated': "123123",
+                        'tags': "1231231",
+                        'creationTime': "123123",
+                        'topicID': "6",
+                        'timezoneOffset': "123123",
+                        'value': "12"
+                    },
+                    {
+                        'id': "9",
+                        'api': "1",
+                        'comment': "qqq",
+                        'end': "123",
+                        'fullTextDescription': "qeqweqw",
+                        'guestId': "1",
+                        'isEmpty': "1",
+                        'objectType': "1",
+                        'start': "1231231",
+                        'apiKeyId': "1",
+                        'timeUpdated': "123123",
+                        'tags': "1231231",
+                        'creationTime': "123123",
+                        'topicID': "7",
+                        'timezoneOffset': "123123",
+                        'value': "14"
+                    }
+                ];
+
             }]);
     }
 
